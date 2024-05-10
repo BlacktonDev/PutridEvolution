53b5da5638f8ac9f6f28b8b45e7533e41b521161
Romo <jantonio.romot@gmail.com>
2024-05-10 01:39:44 -0600

    a

--- Source/PutridEvolution/Private/Character/RomoCharacterBase.cpp
+++ Source/PutridEvolution/Private/Character/RomoCharacterBase.cpp
@@ -8,6 +8,8 @@ ARomoCharacterBase::ARomoCharacterBase()
 	PrimaryActorTick.bCanEverTick = false;
 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ARomoCharacterBase::BeginPlay()
